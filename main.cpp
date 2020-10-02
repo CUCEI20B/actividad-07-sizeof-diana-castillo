210655620d70e1cdb1ac12480c5d943b6589d7d6
Diana Castillo <diana.castillo7676@alumnos.udg.mx>
2020-10-02 04:21:03 -0500

    prueba05

--- main.cpp
+++ main.cpp
@@ -3,25 +3,26 @@
 using namespace std;
 
 int main() {
-    char a;
-    int b;
-    short c;
-    float d;
-    double e;
-    long f;
+    string cadena;
+    getline(cin, cadena);
 
-    cin>>a;
-    cout<<sizeof(a)<<endl;
-    cin>>b;
-    cout<<sizeof(b)<<endl;
-    cin>>c;
-    cout<<sizeof(c)<<endl;
-    cin>>d;
-    cout<<sizeof(d)<<endl;
-    cin>>e;
-    cout<<sizeof(e)<<endl;
-    cin>>f;
-    cout<<sizeof(f)<<endl;
+    if(cadena == "char")
+        cout<<sizeof(char)<<endl;
+
+    if(cadena == "int")
+        cout<<sizeof(int)<<endl;
+
+    if(cadena == "short")
+        cout<<sizeof(short)<<endl;
+
+    if(cadena == "float")
+        cout<<sizeof(float)<<endl;
+
+    if(cadena == "double")
+        cout<<sizeof(double)<<endl;
+
+    if(cadena == "long")
+        cout<<sizeof(long)<<endl;
 
     return 0;
 }
