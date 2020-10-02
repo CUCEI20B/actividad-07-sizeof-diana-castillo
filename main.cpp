c365ca4ec8d86036b5d20ab16746594224c65fe2
Diana Castillo <diana.castillo7676@alumnos.udg.mx>
2020-10-02 03:44:21 -0500

    prueba03

--- main.cpp
+++ main.cpp
@@ -8,10 +8,20 @@ int main() {
     short c;
     float d;
     double e;
-    long g;
-    
+    long f;
+
     cin>>a;
+    cin>>b;
+    cin>>c;
+    cin>>d;
+    cin>>e;
+    cin>>f;
     cout<<sizeof(a);
+    cout<<sizeof(b);
+    cout<<sizeof(c);
+    cout<<sizeof(d);
+    cout<<sizeof(e);
+    cout<<sizeof(f);
 
     return 0;
 }
